cb00af49ad636f225dcee9584ea1508ee48d4218
miampf <miampf@proton.me>
2022-08-20 14:48:03 +0200

    added colons

--- lexer/src/tokens.rs
+++ lexer/src/tokens.rs
@@ -9,7 +9,7 @@ pub enum TokenType{
     // one or more characters
     Exclamation, ExclamationEqual, Equal, EqualEqual, Greater,
     GreaterEqual, GreaterGreater, Lesser, LesserEqual, LesserLesser,
-    Slash,
+    Slash, Colon, ColonColon,
 
     // Type literals and type keywords
     Identifier, Integer, IntegerType, Float, FloatType, Char, CharType,
